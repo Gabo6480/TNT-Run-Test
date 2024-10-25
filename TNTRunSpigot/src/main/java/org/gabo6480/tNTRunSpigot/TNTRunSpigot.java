76af423dfe3d536cf158e985f0e544ba9b02236e
Gabriel Aguirre <gabominecraftero@gmail.com>
2024-10-25 17:26:32 -0600

    Initial commit

---
+++ TNTRunSpigot/src/main/java/org/gabo6480/tNTRunSpigot/TNTRunSpigot.java
@@ -0,0 +1,17 @@
+package org.gabo6480.tNTRunSpigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTRunSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
